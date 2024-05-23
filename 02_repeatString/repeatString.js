e5744220bef1762d9d6ab864397c055994a81397
Hellu World <165349427+Hello-WWWorld@users.noreply.github.com>
2024-05-23 20:54:49 +0800

    redo the problem, remove .skip in spec file and pass all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,13 +1,20 @@
 const repeatString = function(word, times) {
-	if (times < 0) return "ERROR";
-	let string = "";
+	if (typeof times !== 'number' || times < 0) {
+			return "ERROR";
+		}
+		
+	let result = '';
 	for (let i = 0; i < times; i++) {
-		string += word;
+		result = result + word;
+	
+
 
 	}
-	return string;
-};
+	return result;
 
 
+}
+
+repeatString('', 3);
 // Do not edit below this line
 module.exports = repeatString;
