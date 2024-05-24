2bb4839fbdb88d0c7539deb6caead654accf402e
Hellu World <165349427+Hello-WWWorld@users.noreply.github.com>
2024-05-24 20:24:01 +0800

    create a function that adds two integers and the numbers between them and remove the skip part in spec file to enable all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+	finalSum = 0;
+	i = start;
 
-};
+	while(i <= end) {
+		finalSum += i;
+		i++;
+
+	} return finalSum;
+
+}; 
+	
+
+sumAll(1, 4); // returns the sum of 1 + 2 + 3 + 4 which is 10)
+
 
 // Do not edit below this line
 module.exports = sumAll;
