e14c1136b27e2f9bf3d7e8ebeee1ae2a64b6cdcd
Hellu World <165349427+Hello-WWWorld@users.noreply.github.com>
2024-05-24 18:33:38 +0800

    use filter method to remove specified value from the array. pass: 2 tests. also, remove .skip part in .spec file to enable all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,23 @@
-const removeFromArray = function() {
+
+const removeFromArray = function(arr, valueToRemove) {
+
+	result = arr.filter(arr => arr !== 3);
+	return result;
+
 
 };
 
+
+removeFromArray([1, 2, 3, 4], 3); // should remove 3 and return [1,2,4]
+
+
 // Do not edit below this line
 module.exports = removeFromArray;
+
+// TODO:
+//  × multiple values (3 ms)
+//  × removes multiple of the same value (2 ms)
+//  × ignores non present values (1 ms)
+//  × ignores non present values, but still works (1 ms)
+//  × can remove all values
+//  × works with strings (1 ms)
