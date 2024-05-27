55114f5d5624c37d41ab53f973b7b4097717c723
Hellu World <165349427+Hello-WWWorld@users.noreply.github.com>
2024-05-27 15:25:20 +0800

    add century years logic

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,16 +1,18 @@
 const leapYears = function(num) {
 
-	if (num % 2 === 0) {
-		return true;
-
-	} if (num % 4 === 0) {
-		return true;
-
-	} else 
+	if (num % 4 === 0) {
+		if (num % 100 === 0) {
+			if (num % 400 === 0) {
+				return true;
+			} else {
+				return false;
+			}
+		} else 
+			return true;
+	} else
 		return false;
-
 };
-
+			
 leapYears(1999);
 
 // Do not edit below this line
