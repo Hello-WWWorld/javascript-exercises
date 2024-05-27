d593f5ff23691ea6f6146c619c5da30093afdc43
Hellu World <165349427+Hello-WWWorld@users.noreply.github.com>
2024-05-27 15:47:41 +0800

    add temp conversion for fahrenheit and celsius

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+
+	let fh =(fahrenheit - 32) * 5 / 9 
+
+	return fh;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+
+	let c = celsius * 9 / 5 +  32
+
+	return c;
 };
 
 // Do not edit below this line
