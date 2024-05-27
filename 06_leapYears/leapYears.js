de52829b3b6b052c855e5b9582de9b90d9e6d2c3
Hellu World <165349427+Hello-WWWorld@users.noreply.github.com>
2024-05-27 15:03:53 +0800

    add leapYear function to determine leap years

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,17 @@
-const leapYears = function() {
+const leapYears = function(num) {
+
+	if (num % 2 === 0) {
+		return true;
+
+	} if (num % 4 === 0) {
+		return true;
+
+	} else 
+		return false;
 
 };
+
+leapYears(1999);
 
 // Do not edit below this line
 module.exports = leapYears;
