528f2abbf5aeac06cb74182e866721678f22ea36
Hellu World <165349427+Hello-WWWorld@users.noreply.github.com>
2024-05-27 16:00:47 +0800

    add method to round to 1 decimal and remove .skip in spec file to enable all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,15 +1,15 @@
 const convertToCelsius = function(fahrenheit) {
 
-	let fh =(fahrenheit - 32) * 5 / 9 
+	let fh = (fahrenheit - 32) * 5 / 9 
 
-	return fh;
+	return Math.round(fh * 10) / 10;
 };
 
 const convertToFahrenheit = function(celsius) {
 
 	let c = celsius * 9 / 5 +  32
 
-	return c;
+	return Math.round(c * 10) / 10;
 };
 
 // Do not edit below this line
