1cbc0363029dbcfb5f380f532278982a62b49189
Hellu World <165349427+Hello-WWWorld@users.noreply.github.com>
2024-06-14 16:43:52 +0800

    add function to return the oldest person

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldestAge = -Infinity;
+    let oldestPerson = null;
 
+    for (let person of people) {
+        const { name, yearOfBirth, yearOfDeath } = person;
+
+        // Calculate age based on yearOfDeath if available, otherwise use current year
+        const currentYear = new Date().getFullYear();
+        const age = yearOfDeath ? (yearOfDeath - yearOfBirth) : (currentYear - yearOfBirth);
+
+        if (age > oldestAge) {
+            oldestAge = age;
+            oldestPerson = person;
+        }
+    }
+
+    return oldestPerson;
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
