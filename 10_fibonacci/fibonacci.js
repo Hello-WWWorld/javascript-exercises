1772d0fba71d1e1b0180184414f52aff8f473ddb
Hellu World <165349427+Hello-WWWorld@users.noreply.github.com>
2024-06-14 15:39:02 +0800

    add function to return the 4th fibonacci number

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+	if (n <= 0) {
+		return "Error";
+	} else if (n === 1) {
+		return 0;
+	} else if (n === 2) {
+		return 1;
+	}
+
+	let a = 1, b = 2, nextNum; 
+	for (let i = 4; i <= n; i++) {
+		nextNum = a + b;
+		a = b;
+		b = nextNum;
+
+
+	}
+	return nextNum;
+
 
 };
 
