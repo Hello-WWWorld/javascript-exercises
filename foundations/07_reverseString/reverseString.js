50f0161a1bcfb6480ad5f9f84e95f9707e0f5bdf
G <165349427+Hello-WWWorld@users.noreply.github.com>
2026-01-06 03:57:17 +0800

    reverseString: reverse input using split, reverse, join

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(reverseString) {
+    return reverseString.split("").reverse().join(""); 
 };
 
+console.log(reverseString);
 // Do not edit below this line
 module.exports = reverseString;
