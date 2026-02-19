3a41559d55e8353a9ddd8afd8c57fa94c1f8dffe
G <165349427+Hello-WWWorld@users.noreply.github.com>
2026-02-20 07:30:46 +0800

    add temperature conversion functions and enable tests
    - Implement convertToCelsius with 1-decimal rounding
    - Impelement convertToFahrenheit with 1-decimal rounding
    - Ensures returned values are numbers, not strings
    - Removed ".skip" from spec file to enable all tests

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(toCelsius) {
+  return Math.round(((toCelsius - 32) / 1.8) * 10) / 10;
+
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(toFahrenheit) {
+  return Math.round(((1.8 * toFahrenheit) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
