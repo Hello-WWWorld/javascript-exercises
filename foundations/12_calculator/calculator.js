3af564cba746b52704d9a13603ec27cd9d164595
G <165349427+Hello-WWWorld@users.noreply.github.com>
2026-02-20 13:18:25 +0800

    implement all basic math functions and enable all tests
    - added add function to add two numbers
    - added subtact function to subtract two numbers
    - added sum function to sum all elements of an array
    - added multiply function to multiply all elements of an array
    - added power function to raise a number to a power
    - added factorial function to compute the factorial of a number
    - removed ".skip" from spec file to enable all tests

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let total = 0;
+  for (let i = 0; i < array.length; i++) {
+    total += array [i];
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let total = 1;
+  for (let i = 0; i < array.length; i++) {
+    total *= array [i];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let total = 1;
+  for (let i = 1; i <= num; i++) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
